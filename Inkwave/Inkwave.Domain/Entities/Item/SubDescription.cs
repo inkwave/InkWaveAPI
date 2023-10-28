7b1eb3947455f4dbb98333383f0a2f88a0850d4a
MoAbdelfattah <m.a.fattah95@gmail.com>
2023-10-28 19:12:29 +0200

    Retyped SubDescription Properties as a String for all

--- Inkwave/Inkwave.Domain/Entities/Item/SubDescription.cs
+++ Inkwave/Inkwave.Domain/Entities/Item/SubDescription.cs
@@ -5,15 +5,14 @@ namespace Inkwave.Domain.Entities
 {
     public class SubDescription
     {
-        //create a sub description for an item contains ISBN, Author, Publisher, Year, Edition, Pages, Language, Weight, Category, SubCategory, Tags
         public string ISBN { get; set; }
         public string Author { get; set; }
         public string Publisher { get; set; }
-        public int Year { get; set; }
-        public int Edition { get; set; }
-        public int Pages { get; set; }
+        public string Year { get; set; }
+        public string Edition { get; set; }
+        public string Pages { get; set; }
         public string Language { get; set; }
-        public decimal Weight { get; set; }
+        public string Weight { get; set; }
         public string Category { get; set; }
         public string SubCategory { get; set; }
         public string Tags { get; set; }
