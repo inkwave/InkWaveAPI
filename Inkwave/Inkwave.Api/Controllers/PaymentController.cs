cb5c55c15aa713c601f46838ecc342f579928c8e
MoAbdelfattah <m.a.fattah95@gmail.com>
2023-11-15 16:30:19 +0200

    Add Query to Payment Controller

---
+++ Inkwave/Inkwave.Api/Controllers/PaymentController.cs
@@ -0,0 +1,29 @@
+﻿using Inkwave.Application.Features.Favourites.Commands.AddFavourite;
+using Inkwave.Application.Features.Favourites.Commands.RemoveFavourite;
+using Inkwave.Application.Features.Favourites.Queries.GetMyFavourite;
+using Inkwave.Application.Features.Payments.Commands.AddPayment;
+
+
+namespace Inkwave.WebAPI.Controllers
+{
+    [Authorize]
+    public class PaymentController : ApiControllerBase
+    {
+        private readonly IMediator _mediator;
+        public PaymentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost()]
+        
+        public async Task<ActionResult<Result<Guid>>> AddPayment(Guid id)
+        {
+            var command = new AddPaymentCommand() { OrderId = id };
+            return await _mediator.Send(command);
+
+        }
+        
+
+
+    }
+}
