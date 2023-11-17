d49a5f2a123a73eb06f7a58bd5e375dda5a70fa7
Mohamad Salama <mohamadsalama102@outlook.com>
2023-11-17 13:26:37 +0200

    Migration Addresses

---
+++ Inkwave/Inkwave.Presentation/Migrations/20231117112617_updateAddress.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inkwave.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Street = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Building = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apartment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MarkingPlace = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsDefault = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Users_CustomerId",
+                table: "Orders",
+                column: "CustomerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Users_CustomerId",
+                table: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Addresses");
+        }
+    }
+}
